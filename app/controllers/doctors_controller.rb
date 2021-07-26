b2f4e9dd33c04364782641cca86dee18880761cb
github username <kelly.hesterly@gmail.com>
2021-07-26 11:22:25 -0600

    Done.

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,15 @@
+class DoctorsController < ApplicationController
+    def show
+        @doctor = Doctor.find(params[:id])
+    end
+
+    def index
+        @doctor = Doctor.all
+    end
+
+    private
+
+    def doctor_params
+        params.require(:doctor).permit(:name, :department)
+    end
+end
